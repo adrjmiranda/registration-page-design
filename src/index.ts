fdd50f503cc41a5bbe61e2b444bf9a3336f63a1d
Adriano Miranda <adrjmiranda@gmail.com>
2025-02-17 00:59:36 -0300

    chore: added animation to the form

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+/**
+ * ScrollReveal Animation
+ */
+import ScrollReveal from 'scrollreveal';
+
+ScrollReveal().reveal('#form', {
+	reset: false,
+	opacity: 0,
+	duration: 1500,
+	delay: 300,
+	easing: 'ease-in-out',
+});
